18ab570c54f6e53b6baa8df37980c0827f90cb71
Youngjae so <terranbang@gmail.com>
2026-06-18 02:41:19 +0900

    feat: provider와 providerId 복합 유니크 제약 조건 추가

--- src/main/java/com/project/picngo/user/domain/User.java
+++ src/main/java/com/project/picngo/user/domain/User.java
@@ -1,15 +1,6 @@
 package com.project.picngo.user.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,7 +10,14 @@ import java.time.LocalDateTime;
 
 @Getter
 @Entity
-@Table(name = "users")
+@Table(name = "users",
+	uniqueConstraints = {
+			@UniqueConstraint(
+					name = "uk_users_provider_provider_id",
+					columnNames = {"provider", "provider_id"}
+			)
+	}
+)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
 
@@ -47,7 +45,7 @@ public class User {
 	@Column(nullable = false, length = 20)
 	private SocialProvider provider;
 
-	@Column(length = 100)
+	@Column(name = "provider_id", length = 100)
 	private String providerId;
 
 	@Column(nullable = false)
