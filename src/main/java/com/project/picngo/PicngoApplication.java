4061714ce37de2afb8ff98722dfa477a19566e34
Youngjae so <terranbang@gmail.com>
2026-06-17 21:29:58 +0900

    feat: env 파일 임시 로딩 및 TODO 추가

--- src/main/java/com/project/picngo/PicngoApplication.java
+++ src/main/java/com/project/picngo/PicngoApplication.java
@@ -1,5 +1,6 @@
 package com.project.picngo;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,6 +11,17 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class PicngoApplication {
 
 	public static void main(String[] args) {
+
+		// TODO: 하드코딩된 .env 경로를 제거하고 공통 환경변수 로딩 방식으로 대체 고려
+		Dotenv dotenv = Dotenv.configure()
+				.directory("C:/picngo/picngo")
+				.ignoreIfMissing()
+				.load();
+
+		dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+		);
+
 		SpringApplication.run(PicngoApplication.class, args);
 	}
 
