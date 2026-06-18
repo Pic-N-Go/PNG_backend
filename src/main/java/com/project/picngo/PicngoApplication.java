04918b68280e3ec8e63e8c4a458c1269b679b955
Youngjae so <terranbang@gmail.com>
2026-06-18 22:32:02 +0900

    fix: env 파일 임시 로딩 코드 삭제

--- src/main/java/com/project/picngo/PicngoApplication.java
+++ src/main/java/com/project/picngo/PicngoApplication.java
@@ -12,16 +12,6 @@ public class PicngoApplication {
 
 	public static void main(String[] args) {
 
-		// TODO: 하드코딩된 .env 경로를 제거하고 공통 환경변수 로딩 방식으로 대체 고려
-		Dotenv dotenv = Dotenv.configure()
-				.directory("C:/picngo/picngo")
-				.ignoreIfMissing()
-				.load();
-
-		dotenv.entries().forEach(entry ->
-				System.setProperty(entry.getKey(), entry.getValue())
-		);
-
 		SpringApplication.run(PicngoApplication.class, args);
 	}
 
