0d2074416bd8e7795b518508947fc4c0932ea21e
jmmo0722 <moguricamera@gmail.com>
2026-07-01 15:50:55 +0900

    refactor: import문 추가

--- src/main/java/com/project/picngo/notification/repository/NotificationSettingRepository.java
+++ src/main/java/com/project/picngo/notification/repository/NotificationSettingRepository.java
@@ -2,11 +2,14 @@ package com.project.picngo.notification.repository;
 
 import com.project.picngo.notification.domain.NotificationSetting;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface NotificationSettingRepository extends JpaRepository<NotificationSetting, Long> {
     Optional<NotificationSetting> findByUserId(Long userId);
 
-    @org.springframework.data.jpa.repository.Query("SELECT n FROM NotificationSetting n WHERE n.isAllPushEnabled = true AND n.fcmToken IS NOT NULL AND n.fcmToken != ''")
-    java.util.List<NotificationSetting> findActiveSettingsWithToken();
+    @Query("SELECT n FROM NotificationSetting n WHERE n.isAllPushEnabled = true AND n.fcmToken IS NOT NULL AND n.fcmToken != ''")
+    List<NotificationSetting> findActiveSettingsWithToken();
 }
