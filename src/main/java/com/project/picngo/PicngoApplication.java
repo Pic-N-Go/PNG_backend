6b6ec4fdf214ab10c65d98686c32199dc169cad5
jmmo0722 <moguricamera@gmail.com>
2026-07-01 14:00:26 +0900

    refactor: .env 파일 로딩 방식을 Spring Boot 네이티브 기능으로 마이그레이션

--- src/main/java/com/project/picngo/PicngoApplication.java
+++ src/main/java/com/project/picngo/PicngoApplication.java
@@ -1,6 +1,5 @@
 package com.project.picngo;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
