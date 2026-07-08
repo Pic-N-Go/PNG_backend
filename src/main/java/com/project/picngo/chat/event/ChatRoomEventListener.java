1189d1e63cb5470034c1a88ebfcedcfbf2815b62
Youngjae so <terranbang@gmail.com>
2026-07-08 15:50:13 +0900

    refactor: 이벤트 리스너 트랜잭션 보장 처리

--- src/main/java/com/project/picngo/chat/event/ChatRoomEventListener.java
+++ src/main/java/com/project/picngo/chat/event/ChatRoomEventListener.java
@@ -11,6 +11,8 @@ import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
@@ -29,6 +31,7 @@ public class ChatRoomEventListener {
     //TODO: Spot 생성 로직이 구현되었을 때 이벤트 발행하도록 연결
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void handleSpotCreated(SpotCreatedEvent event) {
         chatRoomService.createForSpot(event.spotId());
     }
