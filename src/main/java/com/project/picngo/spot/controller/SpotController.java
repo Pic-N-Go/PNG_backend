16b0334af2fa171746872c01d8ff0468d289ddef
yeainlee <lyy923923@gmail.com>
2026-07-09 22:18:47 +0900

    docs: 스팟 API Swagger 문서 추가

--- src/main/java/com/project/picngo/spot/controller/SpotController.java
+++ src/main/java/com/project/picngo/spot/controller/SpotController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/spots")
 @RequiredArgsConstructor
-public class SpotController {
+public class SpotController implements SpotControllerApiSpec {
 
     private final SpotService spotService;
 
