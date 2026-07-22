eb1575009796f9c2d687e520eef880eb76a54054
yeainlee <lyy923923@gmail.com>
2026-07-22 21:34:43 +0900

    feat: 팔로우 Repository 추가

---
+++ src/main/java/com/project/picngo/user/repository/FollowRepository.java
@@ -0,0 +1,23 @@
+package com.project.picngo.user.repository;
+
+import com.project.picngo.user.domain.Follow;
+import com.project.picngo.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface FollowRepository extends JpaRepository<Follow, Long> {
+
+    // 이미 팔로우한 관계인지 확인
+    boolean existsByFollowerAndFollowing(User follower, User following);
+
+    // 팔로우 관계 조회
+    Optional<Follow> findbyFollowerAndFollowing(User follower, User following);
+
+    // 특정 사용자를 팔로우하는 사람 목록 조회
+    List<Follow> findAllByFollowing(User following);
+
+    // 특정 사용자를 팔로우 중인 사람 목록 조히
+    List<Follow> findAllByFollower(User follower);
+}
