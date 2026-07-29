f6c639e191b241cb15f9d5039b9738f893d3d500
yeainlee <lyy923923@gmail.com>
2026-07-29 22:19:04 +0900

    fix: 팔로우 관계 조회 메서드명 수정(by->By)

--- src/main/java/com/project/picngo/user/repository/FollowRepository.java
+++ src/main/java/com/project/picngo/user/repository/FollowRepository.java
@@ -13,7 +13,7 @@ public interface FollowRepository extends JpaRepository<Follow, Long> {
     boolean existsByFollowerAndFollowing(User follower, User following);
 
     // 팔로우 관계 조회
-    Optional<Follow> findbyFollowerAndFollowing(User follower, User following);
+    Optional<Follow> findByFollowerAndFollowing(User follower, User following);
 
     // 특정 사용자를 팔로우하는 사람 목록 조회
     List<Follow> findAllByFollowing(User following);
