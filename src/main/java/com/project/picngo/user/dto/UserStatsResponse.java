21473e119fa2185d83012f14af1e1e197dad506f
yeainlee <lyy923923@gmail.com>
2026-07-30 19:47:29 +0900

    fix: 오타 수정

--- src/main/java/com/project/picngo/user/dto/UserStatsResponse.java
+++ src/main/java/com/project/picngo/user/dto/UserStatsResponse.java
@@ -1,9 +1,9 @@
 package com.project.picngo.user.dto;
 
-public record UserStatsResponse (
-        long follwerCount,
+public record UserStatsResponse(
+        long followerCount,
         long followingCount,
         long reviewCount,
         long visitedSpotCount
-){
+) {
 }
