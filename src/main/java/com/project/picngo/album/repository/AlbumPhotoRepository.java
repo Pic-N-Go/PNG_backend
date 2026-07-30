55f2c5b26df364d033bdff3abada4a84539b7087
yeainlee <lyy923923@gmail.com>
2026-07-30 20:04:50 +0900

    feat: 앨범 사진 Repository 추가

---
+++ src/main/java/com/project/picngo/album/repository/AlbumPhotoRepository.java
@@ -0,0 +1,16 @@
+package com.project.picngo.album.repository;
+
+import com.project.picngo.album.domain.Album;
+import com.project.picngo.album.domain.AlbumPhoto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AlbumPhotoRepository extends JpaRepository<AlbumPhoto, Long> {
+
+    // 특정 앨범에 포함된 사진 목록 조회
+    List<AlbumPhoto> findAllByAlbum(Album album);
+
+    // 특정 앨범에 포함된 사진 개수 조회
+    long countByAlbum(Album album);
+}
