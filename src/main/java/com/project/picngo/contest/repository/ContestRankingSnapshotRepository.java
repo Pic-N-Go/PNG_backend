e65cc0e3b1e724d3e9a8fd1c70a87d37f6bb21b6
yeainlee <lyy923923@gmail.com>
2026-08-11 20:49:04 +0900

    feat: 콘테스트 순위 집계 repository 추가

---
+++ src/main/java/com/project/picngo/contest/repository/ContestRankingSnapshotRepository.java
@@ -0,0 +1,18 @@
+package com.project.picngo.contest.repository;
+
+import com.project.picngo.contest.domain.Contest;
+import com.project.picngo.contest.domain.ContestRankingSnapshot;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface ContestRankingSnapshotRepository extends JpaRepository<ContestRankingSnapshot, Long> {
+
+    // 특정 콘테스트의 집계 기록 조회
+    List<ContestRankingSnapshot> findAllByContestOrderBySnapshotDateAscRankAsc(Contest contest);
+
+    // 특정 날짜 집계 기록 조회
+    List<ContestRankingSnapshot> findAllByContestAndSnapshotDateOrderByRankAsc(Contest contest, LocalDate snapshotDate);
+
+}
