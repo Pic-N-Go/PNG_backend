62a3008cfa64978b9bb053fd4b5ac7b5e2ae709e
yeainlee <lyy923923@gmail.com>
2026-08-11 20:39:24 +0900

    feat: 콘테스트 출품작 repository 추가

---
+++ src/main/java/com/project/picngo/contest/repository/ContestEntryRepository.java
@@ -0,0 +1,28 @@
+package com.project.picngo.contest.repository;
+
+import com.project.picngo.contest.domain.Contest;
+import com.project.picngo.contest.domain.ContestEntry;
+import com.project.picngo.user.domain.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ContestEntryRepository extends JpaRepository<ContestEntry, Long> {
+
+    // 콘테스트별 출품작 목록 조회
+    Page<ContestEntry> findAllByContest(Contest contest, Pageable pageable);
+
+    // 콘테스트별 내 출품작 목록 조회
+    List<ContestEntry> findAllByContestAndUser(Contest contest, User user);
+
+    // 콘테스트별 내 출품작 수 조회
+    long countByContestAndUser(Contest contest, User user);
+
+    // 콘테스트별 전체 출품작 수 조회
+    long countByContest(Contest contest);
+
+    // 콘테스트별 참여자 수 조회
+    long countDistinctUsersByContest(Contest contest);
+}
