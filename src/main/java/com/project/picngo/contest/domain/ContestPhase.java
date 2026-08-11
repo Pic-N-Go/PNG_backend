7c5bdfcfdff94abac2734e501f38677859c5e89c
yeainlee <lyy923923@gmail.com>
2026-08-11 20:21:03 +0900

    feat: 콘테스트 진행 상태 enum 추가

---
+++ src/main/java/com/project/picngo/contest/domain/ContestPhase.java
@@ -0,0 +1,8 @@
+package com.project.picngo.contest.domain;
+
+public enum ContestPhase {
+    SUBMITTING, // 출품 기간
+    VOTING,     // 투표 기간
+    RESULT,     // 결과 발표 기간
+    ENDED       // 종료된 콘테스트
+}
