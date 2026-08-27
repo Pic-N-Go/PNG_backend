38936f2ccd949699659c17f29e4a21567fa5a9c0
Youngjae so <terranbang@gmail.com>
2026-08-27 20:56:09 +0900

    feat: 채팅 메시지 커서 조회 쿼리 추가

--- src/main/java/com/project/picngo/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/project/picngo/chat/repository/ChatMessageRepository.java
@@ -1,12 +1,16 @@
 package com.project.picngo.chat.repository;
 
 import com.project.picngo.chat.domain.ChatMessage;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
-    List<ChatMessage> findTop50ByChatRoomIdOrderByCreatedAtDesc(Long chatRoomId);
+
+    List<ChatMessage> findByChatRoomIdOrderByIdDesc(Long chatRoomId, Pageable pageable);
+
+    List<ChatMessage> findByChatRoomIdAndIdLessThanOrderByIdDesc(Long chatRoomId, Long beforeId, Pageable pageable);
 
     List<ChatMessage> findTop3ByChatRoomIdOrderByCreatedAtDesc(Long chatRoomId);
 }
